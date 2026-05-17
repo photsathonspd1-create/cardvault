e75302260fed694395b5ed3c87e27a0b344ac268
photsathonspd1-create <photsathonspd1-create@users.noreply.github.com>
2026-05-17 10:36:56 +0800

    fix: add missing SERIES_LABELS import in sell/new page

--- app/sell/new/page.tsx
+++ app/sell/new/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
-import { cn, formatPrice } from "@/lib/utils"
+import { cn, formatPrice, SERIES_LABELS } from "@/lib/utils"
 import Image from "next/image"
 import Link from "next/link"
 import {
